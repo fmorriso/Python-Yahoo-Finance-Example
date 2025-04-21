2c75114fb597e284e63787b554ba95d83146e0b3
Fred Morrison <frederick.morrison@outlook.com>
2025-04-21 11:02:44 -0600

    rough code works

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
+
 import numpy as np
 import yfinance as yf
+import pandas as pd
+# from tabulate import tabulated
+from IPython.display import display
+
 
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
@@ -9,11 +14,28 @@ def get_python_version() -> str:
 def get_numpy_version() -> str:
     return np.__version__
 
+
 def get_yfinance_version() -> str:
     return yf.__version__
 
+
+def get_financial_information(stock_symbol: str):
+    tkr = yf.Ticker(stock_symbol)
+    print(f'{type(tkr) = }')
+
+    print(f'{type(tkr.info) = }')
+    print(tkr.info)
+
+    print(f'{type(tkr.calendar) = }')
+    print(tkr.calendar)
+
+    df = pd.DataFrame(tkr.calendar)
+    display(df)
+
+
 def main():
-    pass
+    get_financial_information('TSLA')
+
 
 if __name__ == '__main__':
     print(f'Python version: {get_python_version()}')
